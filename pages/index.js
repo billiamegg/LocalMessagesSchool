cfc12eab2ff111e5d80225e27085507c9a2421e7
billiamegg <122169777+billiamegg@users.noreply.github.com>
2023-01-26 15:22:28 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hi Big Ben!!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hi Big Ben!!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
